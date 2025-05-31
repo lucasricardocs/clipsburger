742dd0d9b13957dd5df10149fa04bddcf5fb3cbb
lucasricardocs <lucasricardo@gmail.com>
2025-05-31 17:26:10 -0300

    Atualizar o app.py

--- app.py
+++ app.py
@@ -1070,8 +1070,8 @@ def main():
         top: 50%;
         left: 50%;
         transform: translate(-50%, -50%);
-        width: 150px;
-        height: 150px;
+        width: 100px;
+        height: 100px;
         background-image: url('https://raw.githubusercontent.com/lucasricardocs/clipsburger/refs/heads/main/logo.png');
         background-size: contain;
         background-repeat: no-repeat;
